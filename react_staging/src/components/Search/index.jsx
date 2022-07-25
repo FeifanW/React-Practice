69cf56e0596454c5db3d9a28aaacc08b5ebc0068
FeifanW <qihang791@163.com>
2022-07-26 01:30:28 +0800

    练习fetch发送请求

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -3,7 +3,7 @@ import PubSub from 'pubsub-js'
 import axios from 'axios'
 
 class Search extends Component {
-    search = ()=>{
+    search = async ()=>{
         // PubSub.publish('wangwangdui',{name:'tom',age:'18'})
 
 
@@ -13,23 +13,53 @@ class Search extends Component {
         const {keyWordElement:{value}} = this
         // 发送请求前通知app更新状态
         // this.props.updateAppState({isFirst: false,isLoading: true})
-        PubSub.publish('wangwangdui',{isFirst: false,isLoading: true})
-        console.log("value",value)
-        // 发送网络请求
-        axios.get(`http://localhost:3000/api1/search/users?q=${value}`).then(
-            response => {
-                console.log('成功了', response.data)
-                // 请求成功后通知app更新状态
-                // this.props.updateAppState({isLoading: false, users:response.data.items})
-                PubSub.publish('wangwangdui',{isLoading: false, users:response.data.items})
-            },
-            error => {
-                console.log('失败了', error)
-                // 请求失败后通知APP更新状态
-                // this.props.updateAppState({isLoading: false, err:error.message})
-                PubSub.publish('wangwangdui',{isLoading: false, err:error.message})
-            }
-        )
+        // PubSub.publish('wangwangdui',{isFirst: false,isLoading: true})
+
+        // console.log("value",value)
+        // axios发送网络请求
+        // axios.get(`http://localhost:3000/api1/search/users?q=${value}`).then(
+        //     response => {
+        //         console.log('成功了', response.data)
+        //         // 请求成功后通知app更新状态
+        //         // this.props.updateAppState({isLoading: false, users:response.data.items})
+        //         PubSub.publish('wangwangdui',{isLoading: false, users:response.data.items})
+        //     },
+        //     error => {
+        //         console.log('失败了', error)
+        //         // 请求失败后通知APP更新状态
+        //         // this.props.updateAppState({isLoading: false, err:error.message})
+        //         PubSub.publish('wangwangdui',{isLoading: false, err:error.message})
+        //     }
+        // )
+
+        // 发送网络请求---使用fetch发送(未优化版本)
+        // fetch(`/api1/search/user2?q=${value}`).then(
+        //     response => {
+        //         console.log('联系服务器成功了', response)
+        //         return response.json()
+        //     },
+        //     // error => {
+        //     //     console.log('联系服务器失败了',error)
+        //     //     return new Promise()
+        //     // }
+        // ).then(
+        //     response => {console.log("获取数据成功了",response)},
+        //     // error => {console.log('获取数据失败了',error)}
+        // ).catch(
+        //     (error)=>{ console.log(error)}
+        // )
+
+        // 优化版本--使用fetch发送
+        try {
+            const response = await fetch(`/api1/search/users2?q=${value}`)
+            const data = await response.json()
+            console.log('data',data)
+            PubSub.publish('wangwangdui',{isLoading: false,users:data.items})
+        } catch(error){
+            console.log('请求出错',error)
+            PubSub.publish('wangwangdui',{isLoading: false, err:error.message})
+        }
+
     }
     render() {
         return (
